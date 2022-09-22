7d4304f88ecfe5e754f29089186b111c8ec31d2a
levanthuan2001qttl <81482705+levanthuan2001qttl@users.noreply.github.com>
2022-09-22 15:58:42 +0700

    pagination-market: Pagination market overview

--- src/pages/DisplayCoin/components/MarketOverview/MarketOverview.js
+++ src/pages/DisplayCoin/components/MarketOverview/MarketOverview.js
@@ -1,42 +1,57 @@
 import classNames from 'classnames/bind';
-import { Link } from 'react-router-dom';
-import styles from './MarketOverview.module.scss';
-import Image from '~/components/Image/Image';
-import images from '~/assets/images';
-import { faStar } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useEffect, useState } from 'react';
-import PaginationCoin from './Pagination';
-import { Pagination } from 'antd';
+
+import styles from './MarketOverview.module.scss';
+import { marketOverviewService } from '~/services';
+import CoinItem from './CoinItem';
+import ReactPaginate from 'react-paginate';
+import { Spin } from 'antd';
 
 const cx = classNames.bind(styles);
 
-function MarketOverview() {
-    // const [coins, setCoins] = useState([]);
-    // const [loading, setLoading] = useState(false);
-    // const [currentPage, setCurrentPage] = useState(1);
-    // const [coinsPerPage, setCoinsPerPage] = useState(10);
+const PLUS_1 = 1;
 
-    // useEffect(() => {
-    //     const fetchCoin = async () => {
-    //         // setLoading(true);
-    //         const response = await axios.get('http://localhost:4000/display/coins');
-    //         console.log(response.data.coins)
-    //         setCoins(response.data.coins)
-    //         // setLoading(false)
-    //     }
-    //     fetchCoin();
-    // }, []);
+function MarketOverview() {
+    const [marketOverview, setMarketOverview] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [paginationState, setPaginationState] = useState(0);
+
+    const handlePageClick = (selectedItem) => {
+        setPaginationState(selectedItem.selected);
+    };
+
+    useEffect(() => {
+        const fetchCoin = async () => {
+            setLoading(true);
+            // const response = await marketOverviewService.getCoin(paginationState + PLUS_1);
+            // // console.log({ response });
+            // setMarketOverview(response.coins);
+            setTimeout(() => {
+                
+            }, 5000);
+            // setCoins(response.data.coins)
+            setLoading(false);
+        };
+        fetchCoin();
+    }, [paginationState]);
 
     return (
         <section className={cx('colMiddle')}>
             <nav className={cx('statisticsOverview')}>
                 <marquee direction="right">
                     <ul>
-                        <li>Số lượng nhà đầu tư: <span>1,915</span></li>
-                        <li>Tổng số tài sản: <span>$87,951,754,765</span></li>
-                        <li>Tổng số giao dịch 24h: <span>4,138</span></li>
-                        <li>Tổng giá trị các giao dịch 24h: <span>$2,590,054,160</span></li>
+                        <li>
+                            Số lượng nhà đầu tư: <span>1,915</span>
+                        </li>
+                        <li>
+                            Tổng số tài sản: <span>$87,951,754,765</span>
+                        </li>
+                        <li>
+                            Tổng số giao dịch 24h: <span>4,138</span>
+                        </li>
+                        <li>
+                            Tổng giá trị các giao dịch 24h: <span>$2,590,054,160</span>
+                        </li>
                     </ul>
                 </marquee>
                 <div className={cx('row')}>
@@ -45,7 +60,7 @@ function MarketOverview() {
                             <h2>ETH</h2>
                         </div>
                         <div className={cx('search')}>
-                            <input placeholder='search' type='text'></input>
+                            <input placeholder="search" type="text"></input>
                         </div>
                         <select className={cx('filter')}>
                             <option>Filter by price</option>
@@ -68,21 +83,36 @@ function MarketOverview() {
                                     <th>FOLLOW</th>
                                 </tr>
                             </thead>
-                            <tbody className={cx("listCoin")}>
-                                <PaginationCoin />
+                            <tbody className={cx('listCoin')}>
+                                {loading ? (
+                                    <Spin tip="Loading..."> </Spin>
+                                ) : (
+                                    marketOverview.map((coin, index) => (
+                                        <CoinItem index={index} key={coin.id} data={coin} />
+                                    ))
+                                )}
                             </tbody>
                         </table>
+                        <div id={cx('market-table__pagination')}>
+                            <ReactPaginate
+                                previousLabel={'<'}
+                                nextLabel={'>'}
+                                breakLabel={'...'}
+                                breakClassName={cx('break-me')}
+                                pageCount={marketOverview.totalPage || 10}
+                                marginPagesDisplayed={2}
+                                pageRangeDisplayed={5}
+                                onPageChange={handlePageClick}
+                                forcePage={paginationState}
+                                containerClassName={cx('pagination')}
+                                activeClassName={cx('active')}
+                            />
+                        </div>
                     </div>
-                    <Pagination
-                        total={85}
-                        showTotal={(total, range) => `${range[0]}-${range[1]} of ${total} items`}
-                        defaultPageSize={20}
-                        defaultCurrent={1}
-                    />
                 </div>
             </nav>
         </section>
-    )
+    );
 }
 
 export default MarketOverview;
--- src/services/index.js
+++ src/services/index.js
@@ -1,1 +1,2 @@
 export * as authService from './authService';
+export * as marketOverviewService from './marketOverviewService';
