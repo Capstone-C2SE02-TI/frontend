177920e31f428789646626465b3e6d98665c63c5
HieuVo <votrunghieu332001@gmail.com>
2022-12-01 18:08:50 +0700

    TASK-104: OK

--- src/pages/PortfolioSharkFollow/components/PortfolioSharkFollowItem/PortfolioSharkFollowItem.js
+++ src/pages/PortfolioSharkFollow/components/PortfolioSharkFollowItem/PortfolioSharkFollowItem.js
@@ -1,24 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { StarYellowIcon } from '~/components/Icons';
 import classNames from 'classnames/bind';
+import { useDispatch } from 'react-redux';
 import styles from './PortfolioSharkFollowItem.module.scss';
+import { fetchUnFollowSharkWallet } from '~/modules/SharkWallet/sharkWalletSlice';
+import ModalConfirm from '~/layouts/LayoutDefault/components/ModalConfirm';
 import millify from 'millify';
 const cx = classNames.bind(styles);
 
 
-function PortfolioSharkFollowItem({ index, key, dataSharkFollowed }) {
+function PortfolioSharkFollowItem({ key, userId, dataSharkFollowed }) {
+    const [openModal, setOpenModal] = useState(false)
+    const [confirmContent, setConfirmContent] = useState({});
+
+    const dispatch = useDispatch();
+
+    const openModalConfirm = (title, description, type) => {
+        setOpenModal(true)
+        setConfirmContent({ title, description, type });
+    }
+
+    const closeModalConfirm = () => {
+        setOpenModal(false)
+        setConfirmContent({})
+    }
+
+    const handleUnFollow = () => {
+        if (confirmContent.type === 'follow') {
+            dispatch(fetchUnFollowSharkWallet({ userId: userId, sharkId: dataSharkFollowed.id }));
+        }
+    }
+
     return (
         <tr className={cx('portfolio-shark-follow__tr')}>
-            <td>Shark {dataSharkFollowed.id}</td>
+            <td>Shark #{dataSharkFollowed.id}</td>
             <td></td>
             <td>${millify(dataSharkFollowed.totalAssets, {
                 precision: 3,
                 decimalSeparator: ',',
             })}</td>
-            <td></td>
+            <td>{dataSharkFollowed.transactionsHistory.length}</td>
             <td>{dataSharkFollowed.percent24h.toFixed(3) + '%' || '0%'}</td>
             <td></td>
-            <td></td>
+            <td onClick={() => {
+                openModalConfirm('Follow shark', 'Are you sure unfollow this shark?', 'follow');
+            }}
+            >
+                <StarYellowIcon />
+            </td>
+            {openModal && (
+                <ModalConfirm
+                    title={confirmContent.title}
+                    description={confirmContent.description}
+                    modalIsOpen={openModal}
+                    closeModal={closeModalConfirm}
+                    onHandleAction={handleUnFollow}
+                />
+            )}
         </tr>
+
     );
 }
 
