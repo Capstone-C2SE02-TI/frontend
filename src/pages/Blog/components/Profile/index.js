fd9df2c4f4d0837d4e275fac4de806aa70592c09
huynhducthanhtuan <tuan40655@gmail.com>
2023-04-14 15:51:19 +0700

    fix: 🐛 blog profile avatar

--- src/pages/Blog/components/Profile/index.js
+++ src/pages/Blog/components/Profile/index.js
@@ -3,16 +3,19 @@ import { useDispatch, useSelector } from 'react-redux';
 import { getAddressMetaMask } from '~/modules/MetaMask/selector';
 import { setInformationMetaMask } from '~/modules/MetaMask/metaMaskSlice';
 import ConnectWallet from '~/layouts/LayoutDefault/components/ConnectWallet/ConnectWallet';
-import DefaultAvatar from '~/assets/images/DefaultAvatar.png';
 import Image from '~/components/Image/Image';
 import classNames from 'classnames/bind';
 import styles from '../../Blog.module.scss';
+import { userInfoSelector } from '~/modules/user/auth/selectors';
+import { fetchGetUserInfo } from '~/modules/user/auth/authSlice';
 
 const cx = classNames.bind(styles);
 
 function Profile() {
   const dispatch = useDispatch();
   const walletAddress = useSelector(getAddressMetaMask);
+  const userInfo = useSelector(userInfoSelector);
+  const userWalletAddress = localStorage.getItem('eth_address');
   const [isConnecting, setIsConnecting] = useState(false);
 
   const handleGetStatusMeTamask = useCallback(() => {
@@ -31,6 +34,7 @@ function Profile() {
 
   useEffect(() => {
     handleGetStatusMeTamask();
+    dispatch(fetchGetUserInfo(userWalletAddress));
   }, []);
 
   return (
@@ -39,7 +43,7 @@ function Profile() {
         <ConnectWallet handleSetIsConnecting={handleSetIsConnecting} isConnecting={isConnecting} />
       ) : (
         <Fragment>
-          <Image className={cx('profile-image')} src={DefaultAvatar} alt="logo" />
+          <img className={cx('profile-image')} src={userInfo.avatar} alt="logo" />
           <div className={cx('profile-name')}>
             <h5>{walletAddress.slice(0, 10) + '...'}</h5>
           </div>
