95094ca9ed4e95acb870e798a08b264784073555
huynhducthanhtuan <tuan40655@gmail.com>
2023-04-18 16:42:18 +0700

    feat: 🎸 reply comment in blog

--- src/helpers/convertDateToCreatedDate.js
+++ src/helpers/convertDateToCreatedDate.js
@@ -1,15 +1,14 @@
 const convertDateToCreatedDate = (date) => {
   const a = new Date(date);
   const months = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'];
-  
+
   const year = a.getFullYear();
   const month = months[a.getMonth()] < 10 ? '0' + months[a.getMonth()] : months[a.getMonth()];
   const day = a.getDate() < 10 ? '0' + a.getDate() : a.getDate();
   const hour = a.getHours() < 10 ? '0' + a.getHours() : a.getHours();
   const min = a.getMinutes() < 10 ? '0' + a.getMinutes() : a.getMinutes();
-  const sec = a.getSeconds() < 10 ? '0' + a.getSeconds() : a.getSeconds();
 
-  const time = day + '/' + month + '/' + year + ' ' + hour + ':' + min + ':' + sec;
+  const time = day + '/' + month + '/' + year + ' ' + hour + ':' + min;
   return time;
 };
 
