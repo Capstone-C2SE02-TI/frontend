0221595c0635945bfd20ced813a3b4824df35169
HieuVo <votrunghieu332001@gmail.com>
2023-04-21 13:40:45 +0700

    Test

--- src/pages/Blog/Blog.js
+++ src/pages/Blog/Blog.js
@@ -13,6 +13,8 @@ import Image from '~/components/Image/Image';
 import images from '~/assets/images';
 import classNames from 'classnames/bind';
 import styles from './Blog.module.scss';
+import { PowerBIEmbed } from "powerbi-client-react";
+import { models } from "powerbi-client";
 
 const cx = classNames.bind(styles);
 
@@ -49,7 +51,52 @@ function Blog() {
           </div>
         </section>
       </header>
+
       <div className={cx('content-box')}>{blogsByType && renderListBlogs(blogsByType)}</div>
+      <PowerBIEmbed
+        embedConfig={{
+          type: "report", // Supported types: report, dashboard, tile, visual and qna
+          id: "ab867951-2182-4d88-902e-ae047a3a5123",
+          embedUrl: "https://app.powerbi.com/reportEmbed?reportId=ab867951-2182-4d88-902e-ae047a3a5123&groupId=7ae0bb22-58fa-474c-bd98-6c5a7901a8fb&w=2&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly9XQUJJLUlORElBLUNFTlRSQUwtQS1QUklNQVJZLXJlZGlyZWN0LmFuYWx5c2lzLndpbmRvd3MubmV0IiwiZW1iZWRGZWF0dXJlcyI6eyJtb2Rlcm5FbWJlZCI6dHJ1ZSwidXNhZ2VNZXRyaWNzVk5leHQiOnRydWV9fQ%3d%3d",
+          accessToken: "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6Ii1LSTNROW5OUjdiUm9meG1lWm9YcWJIWkdldyIsImtpZCI6Ii1LSTNROW5OUjdiUm9meG1lWm9YcWJIWkdldyJ9.eyJhdWQiOiJodHRwczovL2FuYWx5c2lzLndpbmRvd3MubmV0L3Bvd2VyYmkvYXBpIiwiaXNzIjoiaHR0cHM6Ly9zdHMud2luZG93cy5uZXQvNjJhMjk5ZmUtOGFkZS00MDY0LWIxYWYtZTk4N2Q2ZGNjZGIxLyIsImlhdCI6MTY4MTIxMzM3MCwibmJmIjoxNjgxMjEzMzcwLCJleHAiOjE2ODEyMTc4NzgsImFjY3QiOjAsImFjciI6IjEiLCJhaW8iOiJBVlFBcS84VEFBQUE1Q0ZWK1F6YTBDUGZVNTZLUU11enNySGhhdTV5OExrMStmdzUxeG9pZ3dLN1pvK2d6a2kvZmdFdEphYjR6VTZNYTNXbGFsSFRvSDNDcjd1Q3B0NC93S2RPZkNoT0pHRGxWeXAyMjFXTjN3WT0iLCJhbXIiOlsicHdkIiwibWZhIl0sImFwcGlkIjoiODcxYzAxMGYtNWU2MS00ZmIxLTgzYWMtOTg2MTBhN2U5MTEwIiwiYXBwaWRhY3IiOiIwIiwiZmFtaWx5X25hbWUiOiJIdXluaCIsImdpdmVuX25hbWUiOiJIaWV1IiwiaXBhZGRyIjoiMTAzLjE1Ni41OC4yMTIiLCJuYW1lIjoiSGlldSBIdXluaCIsIm9pZCI6ImVlODJmNmY4LTBjYjYtNDE4Yi1iNzU2LWE0NGY3ZmVhY2I5MCIsInB1aWQiOiIxMDAzMjAwMjkzM0ZFRUM0IiwicmgiOiIwLkFVb0FfcG1pWXQ2S1pFQ3hyLW1IMXR6TnNRa0FBQUFBQUFBQXdBQUFBQUFBQUFDSkFKSS4iLCJzY3AiOiJ1c2VyX2ltcGVyc29uYXRpb24iLCJzaWduaW5fc3RhdGUiOlsia21zaSJdLCJzdWIiOiJETU5iRjlEWWdnMTY4aHFfalJsYXNxMlFtWVlBVW0teHA2cXdHNmUyTzdvIiwidGlkIjoiNjJhMjk5ZmUtOGFkZS00MDY0LWIxYWYtZTk4N2Q2ZGNjZGIxIiwidW5pcXVlX25hbWUiOiJIaWV1SHV5bmhAaGlldWh1eW5oODkwLm9ubWljcm9zb2Z0LmNvbSIsInVwbiI6IkhpZXVIdXluaEBoaWV1aHV5bmg4OTAub25taWNyb3NvZnQuY29tIiwidXRpIjoiMHFMSFM1NmdfRW1VZW93MGNTQXNBUSIsInZlciI6IjEuMCIsIndpZHMiOlsiNjJlOTAzOTQtNjlmNS00MjM3LTkxOTAtMDEyMTc3MTQ1ZTEwIiwiYjc5ZmJmNGQtM2VmOS00Njg5LTgxNDMtNzZiMTk0ZTg1NTA5Il19.BXWPMjGxB9Quh3OOd0gSOIvET672TCRRxxnhVD29YXjjLVXJYXhQnW1LMnqSABu_aIRBQih9Qu3WBj7ixALBEs3v88IOx8KRL_xHHTKT0xPzq7SAyrA77E3fFCYFHHlIAs5_amh_5qcf0SY0-zzkyvTadUeDFu4kiqRmJCvEIou1SYjF4Qa7Dn4m95K9Ep84U71iBvkhHSsc4JJZfGJ9VeuhblOCoQOlILgN0S7jQWz9f4JmwpwDO5nDmEqct1gvMjjA51ezPSuxCD4Etg6NuT-CTx7RyaEqqgfamSj0xP6ErhEXwn2LyDX_2Ea8BL1sduNkIKHCKZd3tpvsf_nf3w",
+          tokenType: models.TokenType.Aad,
+          settings: {
+            panes: {
+              filters: {
+                expanded: false,
+                visible: false,
+              },
+            },
+            background: models.BackgroundType.Transparent,
+          },
+        }}
+        eventHandlers={
+          new Map([
+            [
+              "loaded",
+              function () {
+                console.log("Report loaded");
+              },
+            ],
+            [
+              "rendered",
+              function () {
+                console.log("Report rendered");
+              },
+            ],
+            [
+              "error",
+              function (event) {
+                console.log(event.detail);
+              },
+            ],
+          ])
+        }
+        cssClassName={"report-style-class"}
+        getEmbeddedComponent={(embeddedReport) => {
+          window.report = embeddedReport;
+        }}
+      />
       <BlogFooter />
     </div>
   );
--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -1,123 +1,18 @@
 import classNames from 'classnames/bind';
 import styles from './Profile.module.scss';
 
-import RecommendAccountFollow from '../EditProfile/containers/RecommendAccountFollow';
-import { useDispatch, useSelector } from 'react-redux';
-import { userInfoSelector, userIsPremiumSelector } from '~/modules/user/auth/selectors';
-import { useEffect } from 'react';
-import { fetchGetUserInfo } from '~/modules/user/auth/authSlice';
-import { useNavigate } from 'react-router-dom';
-import Button from '~/components/Button';
-import images from '~/assets/images';
-
 const cx = classNames.bind(styles);
 function Profile() {
-    const dispatch = useDispatch();
-    const userInfoDetail = useSelector(userInfoSelector);
-    const { userId } = JSON.parse(localStorage.getItem('userInfo'));
-    const navigate = useNavigate();
-    const ethAddress = localStorage.getItem('eth_address');
-
-    useEffect(() => {
-        if (ethAddress) {
-            dispatch(fetchGetUserInfo(ethAddress));
-        }
-    }, [dispatch, ethAddress]);
-
-    const renderProfile = () => {
-        return (
-            <div className={cx('wrapper')}>
-                <div className={cx('profile')}>
-                    <div className={cx('profile-avatar')}>
-                        <div className={cx('profile-avatar-detail')}>
-                            <img src={userInfoDetail.avatar || images.userAvatar} alt="" />
-                        </div>
-                        <span>{userInfoDetail.fullName || ''}</span>
-                    </div>
-
-                    <div className={cx('profile-info')}>
-                        <span className={cx('profile-info-username')}>
-                            WalletAddress: <span className="font-italic">{ethAddress.slice(0,16)}...</span>
-                        </span>
-                        <p className={cx('profile-info-email')}> {userInfoDetail.email}</p>
-                        <div className="d-flex justify-content-between">
-                            <div className={cx('profile-info-follow')} style={{ cursor: 'pointer' }}>
-                                <span>0</span>
-                                <p>Following</p>
-                            </div>
-                            <div className={cx('profile-info-follow')} style={{ cursor: 'pointer' }}>
-                                <span>0</span>
-                                <p>Follower</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className={cx('profile-edit')} onClick={() => navigate('/edit-profile')}>
-                        <img
-                            src="https://s2.coinmarketcap.com/static/cloud/img/icon/editor.svg?_=b8777e5"
-                            alt="editorIcon"
-                        />
-                        <span>Edit</span>
-                    </div>
-                </div>
-                <div className="mt-8 p-8" style={{ marginLeft: '36px', marginTop: '50px' }}>
-                    <p className={cx('profile-detail-title')}>
-                        Phone number:
-                        <span className={cx('profile-detail-value')}>{userInfoDetail.phoneNumber}</span>
-                    </p>
-
-                    <p className={cx('profile-detail-title')}>
-                        Created date:
-                        <span className={cx('profile-detail-value')}>
-                            {userInfoDetail?.createdAt?.split('T')[0] || ''}
-                        </span>
-                    </p>
-                    <p className={cx('profile-detail-title')}>
-                        Updated date:
-                        <span className={cx('profile-detail-value')}>
-                            {userInfoDetail?.updatedAt?.split('T')[0] || ''}
-                        </span>
-                    </p>
-                    <p className={cx('profile-detail-title')}>
-                        My website:
-                        <span className={cx('profile-detail-value')}>
-                            <a
-                                href={userInfoDetail.website || ''}
-                                rel="noopener noreferrer"
-                                target="_blank"
-                                style={{ color: '#1890ff' }}
-                            >
-                                {userInfoDetail.website?.split('/')[2] || ''}
-                            </a>
-                        </span>
-                    </p>
-                </div>
-         
-                <div className={cx('d-flex flex-column align-items-center')}>
-                    <img
-                        width="216"
-                        src="https://s2.coinmarketcap.com/static/cloud/img/posts/no-post.png?_=b8777e5"
-                        alt="nothing-here"
-                    />
-                    <p style={{ margin: '0px', color: 'rgb(34, 37, 49)', fontSize: '28px', fontWeight: ' 700' }}>
-                        Nothing here!
-                    </p>
-                    <p style={{ color: 'rgb(128, 138, 157)' }}>
-                        You can discover and follow shark accounts that interest you! If you update your premium
-                        account!
-                    </p>
-                    {/* <Button primary>Create new Post</Button> */}
-                </div>
-            </div>
-        );
-    };
-    const userIsPremium = useSelector(userIsPremiumSelector);
 
     return (
-        <div className="d-flex justify-content-between">
-            {renderProfile()}
-            {userIsPremium && <RecommendAccountFollow />}
+        <div className={cx('wrapper')}>
+            <div className={cx('header_profile')}>
+                <h1>Portfolio shark followed</h1>
+            </div>
         </div>
     );
+
+
 }
 
 export default Profile;
