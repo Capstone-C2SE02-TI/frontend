eb8161fff638dfacf338ff39ca09b707c92a3ecd
Kurosaki Aki <kurosakiaki890@gmail.com>
2023-05-23 15:59:30 +0700

    fetch

--- src/pages/ListTradingSharkAdd/ListTradingSharkAdd.js
+++ src/pages/ListTradingSharkAdd/ListTradingSharkAdd.js
@@ -1,114 +1,90 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import classNames from 'classnames/bind';
 import styles from './ListTradingSharkAdd.module.scss';
 import { io } from 'socket.io-client';
 import Button from '~/components/Button/Button';
+import { useSelector } from 'react-redux';
+import { getAddressMetaMask } from '~/modules/MetaMask/selector';
 
 const cx = classNames.bind(styles);
 
 const ListTradingSharkAdd = () => {
   const socket = io('http://localhost:4001/');
 
-  let pairAutoList = [];
+  const [pairAutoList, setPairAutoList] = useState([]);
+
+  const walletAddress = useSelector(getAddressMetaMask);
 
   useEffect(() => {
-    socket.on("pair-transactions", (data) => {
-      pairAutoList = data;
-      console.log(pairAutoList);
-    })
+    async function fetchPairList() {
+      const response = await fetch(`http://localhost:4000/trading/list`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          userAddress: walletAddress,
+        }),
+      });
 
-  }, []);
+      const result = await response.json();
+      if (result.message === 'success') {
+        setPairAutoList(result.data);
+      }
+    }
+    fetchPairList();
 
-  console.log(pairAutoList);
+    socket.on('pair-transactions', (data) => {
+      setPairAutoList(data);
+    });
+  }, [walletAddress]);
 
   return (
-    <div className={cx("ListTradingSharkAdd")}>
+    <div className={cx('ListTradingSharkAdd')}>
       <h1>List shark auto trading</h1>
-      <table className={cx("ListTradingSharkAdd-table")}>
+      <table className={cx('ListTradingSharkAdd-table')}>
         <thead>
-          <tr className={cx("ListTradingSharkAdd-tr")}>
-            <th className={cx("ListTradingSharkAdd-th")}>
-              SHARK#
-            </th>
-            <th className={cx("ListTradingSharkAdd-th")}>
-              FROM TOKEN
-            </th>
-            <th className={cx("ListTradingSharkAdd-th")}>
-              TO TOKEN
-            </th>
-            <th className={cx("ListTradingSharkAdd-th")}>
-              ETH SENDED
-            </th>
-            <th className={cx("ListTradingSharkAdd-th")}>
-              STATUS
-            </th>
-            <th className={cx("ListTradingSharkAdd-th")}>
-              ACTION
-            </th>
+          <tr className={cx('ListTradingSharkAdd-tr')}>
+            <th className={cx('ListTradingSharkAdd-th')}>SHARK#</th>
+            <th className={cx('ListTradingSharkAdd-th')}>FROM TOKEN</th>
+            <th className={cx('ListTradingSharkAdd-th')}>TO TOKEN</th>
+            <th className={cx('ListTradingSharkAdd-th')}>ETH SENDED</th>
+            <th className={cx('ListTradingSharkAdd-th')}>STATUS</th>
+            <th className={cx('ListTradingSharkAdd-th')}>ACTION</th>
           </tr>
         </thead>
         <tbody>
-          <tr className={cx("ListTradingSharkAdd-tr")}>
-            <td className={cx("ListTradingSharkAdd-td")}>
-              <p className={cx("ListTradingSharkAdd-p")}>Shark 1</p>
-              <span className={cx("ListTradingSharkAdd-span")}>0xa946592bee76E7258444d053773f51e366B34c3C</span>
-            </td>
-            <td className={cx("ListTradingSharkAdd-td")}>
-              <span className={cx("ListTradingSharkAdd-span")}>symbol token from</span>
-            </td>
-            <td className={cx("ListTradingSharkAdd-td")}>
-              <span className={cx("ListTradingSharkAdd-span")}>symbol token to</span>
-            </td>
-            <td className={cx("ListTradingSharkAdd-td")}>
-              <span className={cx("ListTradingSharkAdd-span")}>So luong eth sender</span>
-            </td>
-            <td className={cx("ListTradingSharkAdd-td")}>
-              <span className={cx("ListTradingSharkAdd-span")}>Success</span>
-            </td>
-            <td className={cx("ListTradingSharkAdd-td")}>
-              <Button error className={cx("btn-delete--auto")}>Delete</Button>
-            </td>
-          </tr>
-          <tr className={cx("ListTradingSharkAdd-tr")}>
-            <td className={cx("ListTradingSharkAdd-td")}>
-              <p className={cx("ListTradingSharkAdd-p")}>Shark 1</p>
-              <span className={cx("ListTradingSharkAdd-span")}>0xa946592bee76E7258444d053773f51e366B34c3C</span>
-            </td>
-            <td className={cx("ListTradingSharkAdd-td")}>
-              <span className={cx("ListTradingSharkAdd-span")}>symbol token from</span>
-            </td>
-            <td className={cx("ListTradingSharkAdd-td")}>
-              <span className={cx("ListTradingSharkAdd-span")}>symbol token to</span>
-            </td>
-            <td className={cx("ListTradingSharkAdd-td")}>
-              <span className={cx("ListTradingSharkAdd-span")}>So luong eth sender</span>
-            </td>
-            <td className={cx("ListTradingSharkAdd-td")}>
-              <span className={cx("ListTradingSharkAdd-span")}>Success</span>
-            </td>
-          </tr>
-          <tr className={cx("ListTradingSharkAdd-tr")}>
-            <td className={cx("ListTradingSharkAdd-td")}>
-              <p className={cx("ListTradingSharkAdd-p")}>Shark 1</p>
-              <span className={cx("ListTradingSharkAdd-span")}>0xa946592bee76E7258444d053773f51e366B34c3C</span>
-            </td>
-            <td className={cx("ListTradingSharkAdd-td")}>
-              <span className={cx("ListTradingSharkAdd-span")}>symbol token from</span>
-            </td>
-            <td className={cx("ListTradingSharkAdd-td")}>
-              <span className={cx("ListTradingSharkAdd-span")}>symbol token to</span>
-            </td>
-            <td className={cx("ListTradingSharkAdd-td")}>
-              <span className={cx("ListTradingSharkAdd-span")}>So luong eth sender</span>
-            </td>
-            <td className={cx("ListTradingSharkAdd-td")}>
-              <span className={cx("ListTradingSharkAdd-span")}>Success</span>
-            </td>
-          </tr>
+          {pairAutoList.map((data) => {
+            return (
+              <tr className={cx('ListTradingSharkAdd-tr')}>
+                <td className={cx('ListTradingSharkAdd-td')}>
+                  <p className={cx('ListTradingSharkAdd-p')}>{data.sharkId}</p>
+                  <span className={cx('ListTradingSharkAdd-span')}>{data.sharkAddess}</span>
+                </td>
+                <td className={cx('ListTradingSharkAdd-td')}>
+                  <span className={cx('ListTradingSharkAdd-span')}>{data.fromSymbol}</span>
+                </td>
+                <td className={cx('ListTradingSharkAdd-td')}>
+                  <span className={cx('ListTradingSharkAdd-span')}>{data.toSymbol}</span>
+                </td>
+                <td className={cx('ListTradingSharkAdd-td')}>
+                  <span className={cx('ListTradingSharkAdd-span')}>{data.ethAmount}</span>
+                </td>
+                <td className={cx('ListTradingSharkAdd-td')}>
+                  <span className={cx('ListTradingSharkAdd-span')}>{data.status? "True": "False"}</span>
+                </td>
+                <td className={cx('ListTradingSharkAdd-td')}>
+                  <Button error className={cx('btn-delete--auto')}>
+                    Delete
+                  </Button>
+                </td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
     </div>
-  )
+  );
 };
 
 export default ListTradingSharkAdd;
