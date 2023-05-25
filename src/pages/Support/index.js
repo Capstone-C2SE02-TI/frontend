9c245a2c86100e8bc5611dfb9ccc2efd7405e87f
HieuVo <votrunghieu332001@gmail.com>
2023-05-25 20:35:03 +0700

    fix release v1

--- src/pages/Support/index.js
+++ src/pages/Support/index.js
@@ -1,9 +1,9 @@
 import Tippy from '@tippyjs/react';
 
 function Support() {
-    
 
-const defaultProps = {
+
+  const defaultProps = {
     animateFill: false,
     animation: 'scale',
     interactive: true,
@@ -11,43 +11,43 @@ const defaultProps = {
     theme: 'tomato',
     // trigger: 'click',
     content: 'this is popover content',
-};
+  };
 
 
-    return (
-        <div className={('container')}>
-            <div className={('loader')}>
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-            </div>
-          </div>
-    );
+  return (
+    <div className={('container')}>
+      <div className={('loader')}>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </div>
+  );
 }
 
 export default Support;
